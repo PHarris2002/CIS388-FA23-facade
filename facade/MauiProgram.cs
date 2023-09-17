06a1aa41513c8871f1613a91c46dcd7794eacf93
Priscilla Harris <89226780+PHarris2002@users.noreply.github.com>
2023-09-17 00:55:08 -0500

    9/17 12:55am

--- facade/MauiProgram.cs
+++ facade/MauiProgram.cs
@@ -13,7 +13,8 @@ public static class MauiProgram
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-			});
+                fonts.AddFont("CuteLetters.ttf", "CuteLetters");
+            });
 
 
 		builder.Services.AddTransient<GameOverPage>();
