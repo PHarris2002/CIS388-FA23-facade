e89bced3b4a40ffac66474da8bf951aea1fed829
Priscilla Harris <89226780+PHarris2002@users.noreply.github.com>
2023-09-27 00:16:47 -0500

    9/27 12:16am - Working Delete Button

---
+++ facade/MainPageViewModel.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Diagnostics.Metrics;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace facade
+{
+    public partial class MainPageViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private string secretColor;
+
+        [ObservableProperty]
+        private string currentGuess;
+
+        public ObservableCollection<ColorGuess> Guesses { get; set; }
+
+        //public string SecretColor { get; set; }
+
+        public MainPageViewModel()
+        {
+            secretColor = "FACADE";
+            currentGuess = "";
+
+            Guesses = new ObservableCollection<ColorGuess>();
+
+            Guesses.Add(new ColorGuess("#beaded"));
+            Guesses.Add(new ColorGuess("#efaced"));
+
+        }
+
+
+        [RelayCommand]
+        void AddLetter(string letter)
+        {
+            if (CurrentGuess.Length < 6)
+            {
+                CurrentGuess += letter;
+            }
+        }
+
+        [RelayCommand]
+        void DeleteLetter()
+        {
+            string newCurrentGuess = "";
+            if (CurrentGuess.Length > 0)
+            {
+                newCurrentGuess = CurrentGuess.Substring(0, CurrentGuess.Length - 1);
+            }
+
+            CurrentGuess = newCurrentGuess;
+        }
+
+        [RelayCommand]
+        void Guess()
+        {
+            // if correct, then go to game over (DidWin=true)
+
+            // else if this is the 6th guess (and it's wrong)
+            // then go to game over (DidWin=false)
+
+
+            // Add this guess to the Guesses
+            Guesses.Add(new ColorGuess(CurrentGuess));
+
+        }
+
+
+    }
+}
